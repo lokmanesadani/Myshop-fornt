ebcee29cb1edf87a43bd7d3ed275a2e45e8d5f2f
unknown <hl_sadani@esi.dz>
2023-04-04 17:28:17 +0100

    routes fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import Home from "pages/Home/Home";
 import { Box, styled, ThemeProvider } from "@mui/material";
@@ -6,15 +10,25 @@ import theme from "theme/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import NavBarLayout from "layouts/NavBarLayout";
 const router = createBrowserRouter([
+  // route to rediect to home page
+
   {
     path: "/store",
     element: <NavBarLayout />,
     children: [
       {
+        path: "/store",
+        element: <Navigate to="/store/home" />,
+      },
+      {
         path: "/store/home",
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "*",
+    element: <Navigate to="/store/home" />,
   },
 ]);
 
